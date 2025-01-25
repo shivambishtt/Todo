2485549392e52c30069794ed50ed1e7d67315bb6
Shivam Bisht <rajshivam2745@gmail.com>
2025-01-25 19:26:48 +0530

    add offline feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 function App() {
+  const [online, setOnline] = useState<boolean>(navigator.onLine)
+
+
+  useEffect(() => {
+    const handleOnline = () => {
+      setOnline(true)
+    }
+    const handleOffline = () => {
+      setOnline(false)
+    }
+
+
+    window.addEventListener("online", handleOnline)
+    window.addEventListener("offline", handleOffline)
+
+
+    return () => {
+      window.removeEventListener("online", handleOnline)
+      window.removeEventListener("offline", handleOffline)
+    }
+  }, [])
+
+
   return (
-    <div>
-      <h1>Helo</h1>
+
+    <div className='flex justify-center items-center my-7'>
+      <div className='bg-gray-600 w-auto text-white' >
+        <h1>
+          {online ? "" : "OOPS! Check your internet connection 👀"}
+        </h1>
+      </div>
     </div>
+
   )
 }
 
